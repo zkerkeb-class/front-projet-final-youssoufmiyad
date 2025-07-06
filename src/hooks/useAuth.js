01a09c7735ef579732e365a84dc0eb932010535d
youssoufmiyad <youssoufmiyad@gmail.com>
2025-07-06 04:43:38 +0200

    lien recipe - user dans le hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 // src/hooks/useAuth.js
 import { useState } from "react";
 import api from "../utils/axios"; // ton instance avec token auto
+import { getRecipesByUserId, getSavedRecipesByUserId } from "../utils/users";
 
 export function useAuth() {
   const [token, setToken] = useState(() => localStorage.getItem("token"));
@@ -30,11 +31,45 @@ export function useAuth() {
     localStorage.removeItem("user");
   };
 
+  const addRecipeToUser = async (recipeId) => {
+    if (!token) {
+      throw new Error("Utilisateur non authentifié");
+    }
+    try {
+      const res = await api.post(`/users/${user._id}/add-recipe`, { recipeId });
+      setUser(res.data);
+      localStorage.setItem("user", JSON.stringify(res.data));
+    } catch (err) {
+      throw new Error(
+        err.response?.data?.message || "Erreur lors de l'ajout de la recette"
+      );
+    }
+  };
+
+  const getSavedRecipes = async () => {
+    if (!user || !user.savedRecipes) {
+      return [];
+    }
+    let recipes = await getSavedRecipesByUserId(user._id);
+    return recipes || [];
+  };
+
+  const getRecipes = async () => {
+    if (!user || !user.recipes) {
+      return [];
+    }
+    let recipes = await getRecipesByUserId(user._id);
+    return recipes || [];
+  };
+
   return {
     token,
     user,
     login,
     logout,
+    addRecipeToUser,
+    getSavedRecipes,
+    getRecipes,
     isAuthenticated: !!token,
   };
 }
